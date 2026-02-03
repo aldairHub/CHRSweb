027c894990e709d7778e6e6bca8a29f24890d6c4
eduaardok <eduardoreinoso27@gmail.com>
2026-02-03 10:27:21 -0500

    Configs

--- backend/src/main/java/org/uteq/backend/Config/SecurityConfig.java
+++ backend/src/main/java/org/uteq/backend/Config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/usuarios", "/api/auth/login").permitAll()
                         .anyRequest().authenticated()
+                        //hjfgffyuuy
                 );
 
         return http.build();
